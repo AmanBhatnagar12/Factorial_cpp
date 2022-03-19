83d27a86c49b62369dd762bbc83b9c3a7db4ecc0
Aman Bhatnagar <93813737+AmanBhatnagar12@users.noreply.github.com>
2022-03-19 10:24:50 +0530

    Update factiorial.cpp

--- factiorial.cpp
+++ factiorial.cpp
@@ -4,11 +4,12 @@ int main(){
     int n;
     // Taking variables 
     long double fact=1.0; 
+    // Accepting input 
     cout<<"Enter a positive integer: ";
     cin>>n;
 
     if(n<0) cout<<"Factorial isn't for negatives."<<endl;
-    else {
+    else { // Prefix : Change First then Use 
     for(int i=1; i<=n; ++i) {
         fact *= i;
     }
